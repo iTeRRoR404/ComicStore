9e079e605cf68e513a854706f32097276f4dc593
iTeRRoR404 <sbuzin@bk.ru>
2024-03-11 23:45:29 +0300

    Initial commit

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,78 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://www.28oi.ru/");
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void loginTest() {
+        driver.findElement(By.linkText("Личный кабинет")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("sbuzin@bk.ru");
+        driver.findElement(By.cssSelector("#password")).sendKeys("GdAtA123");
+        driver.findElement(By.cssSelector(".js-co-login-submit")).click();
+        String expectedTitle = "История заказов";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @Test
+    public void loginEmptyPasswordTest() {
+        driver.findElement(By.linkText("Личный кабинет")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("sbuzin@bk.ru");
+        driver.findElement(By.cssSelector("#password")).sendKeys("");
+        driver.findElement(By.cssSelector(".js-co-login-submit")).click();
+        //String expectedTitle = "Заполните это поле.";
+        //Alert alert = driver.switchTo().alert();
+        //String text = alert.getText();
+        //System.out.println(text);
+        //Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @Test
+    public void loginEmptyEmailTest() {
+        driver.findElement(By.linkText("Личный кабинет")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("");
+        driver.findElement(By.cssSelector("#password")).sendKeys("GdAtA123");
+        driver.findElement(By.cssSelector(".js-co-login-submit")).click();
+        //String expectedTitle = "Заполните это поле.";
+        //Alert alert = driver.switchTo().alert();
+        //String text = alert.getText();
+        //System.out.println(text);
+        //Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @Test
+    public void loginInvalidPasswordTest() {
+        driver.findElement(By.linkText("Личный кабинет")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("sbuzin@bk.ru");
+        driver.findElement(By.cssSelector("#password")).sendKeys("GdAtA");
+        driver.findElement(By.cssSelector(".js-co-login-submit")).click();
+        String expectedText = "Сочетание логина и пароля не подходит";
+        String actualText = driver.findElement(By.cssSelector(".co-notice--danger")).getText();
+        Assert.assertEquals(actualText, expectedText);
+    }
+    @Test
+    public void loginInvalidEmailTest() {
+        driver.findElement(By.linkText("Личный кабинет")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("sbuz@bk.ru");
+        driver.findElement(By.cssSelector("#password")).sendKeys("GdAtA123");
+        driver.findElement(By.cssSelector(".js-co-login-submit")).click();
+        String expectedText = "Сочетание логина и пароля не подходит";
+        String actualText = driver.findElement(By.cssSelector(".co-notice--danger")).getText();
+        Assert.assertEquals(actualText, expectedText);
+    }
+
+
+}
---
+++ src/test/java/MainPageTests.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MainPageTests {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://www.28oi.ru/");
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void homePageTitle() {
+        String expectedTitle = "Магазин комиксов \"Двадцать восьмой\"";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @Test
+    public void deliveryPage() {
+        driver.findElement(By.linkText("Доставка и оплата")).click();
+        String expectedTitle = "Доставка и оплата";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @Test
+    public void bonusPage() {
+        driver.findElement(By.linkText("Бонусная система")).click();
+        String expectedTitle = "Накопительная система бонусных баллов";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @Test
+    public void cartPage() {
+        driver.findElement(By.cssSelector(".shopcart-widget-link:nth-child(1) .shopcart-widget-caption")).click();
+        String expectedTitle = "Корзина";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+}
